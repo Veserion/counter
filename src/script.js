52f5c3e6d6355870c670af0d7b1b515b0060221e
Алешенька:3 <alexnagorny.an@gmail.com>
2019-04-13 20:27:38 +0300

    some code

--- src/script.js
+++ src/script.js
@@ -1,30 +1,27 @@
+const getCookies = () => JSON.parse($.cookie('list_values') || JSON.stringify([])) ;
+const setCookies = (cookie) =>  $.cookie('list_values', JSON.stringify(cookie));
+
+const PopUpShow = () => $("#addForm").show();
+const PopUpHide = () => $("#addForm").hide();
+
 $(document).ready(function () {
     PopUpHide();
+    refresh();
+
 });
 
-
-// function createRow(){
-//     var row = new NewRow();
-// }
-
-
-
-function PopUpShow() {
-    $("#addForm").show();
-}
-
-function PopUpHide() {
-    $("#addForm").hide();
-}
-
-function NewRow() {
-    let cookies = JSON.parse($.cookie('list_values')) || JSON.stringify([]);
-    console.log(cookies)
-    let formValues = conventArray($('#addForm').serializeArray());
+function insertRow() {
+    const cookies = getCookies();
+    const formValues = conventArray($('#addForm').serializeArray());
     cookies.push(formValues);
-    $.cookie('list_values', JSON.stringify(cookies));
-    console.log(cookies)
-    // $('#outTable').append('<tr><td>my data</td><td>more data</td><td>more data</td></tr>');
+    setCookies(cookies);
+    refresh();
+}
+
+function removeRow(index) {
+    const cookies = getCookies();
+    cookies.splice(index, 1);
+    setCookies(cookies);
 
 }
 
@@ -32,4 +29,13 @@ function conventArray(data) {
     let out = { };
     data.forEach(({name, value}) => out[name] = value);
     return out
+}
+
+function refresh() {
+    const title = '<tr ><td>id</td><td>Дата</td><td>Сумма</td><td>Комментарий</td><tr>';
+    const cookies = getCookies();
+    const data = cookies.map((item,index) =>
+        `<tr id="${index}"><td>${index}</td>><td>${item.date}</td><td>${item.cost}</td><td>${item.comment}</td></tr>`
+    );
+    $('#outTable').html(`${title}${data}`);
 }
